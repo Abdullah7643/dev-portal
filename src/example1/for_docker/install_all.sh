8874f57a81774b85039511fd03924d0463546c4f
Abdullah7643 <61559027+Abdullah7643@users.noreply.github.com>
2024-05-24 19:03:49 +0500

    Update install_all.sh

--- src/example1/for_docker/install_all.sh
+++ src/example1/for_docker/install_all.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get -y install sudo wget git python3-pip  nodejs npm libudev-dev libusb-1.0-0-dev psmisc python3-full
+apt-get -y install sudo wget git python3-pip  nodejs npm libudev-dev libusb-1.0-0-dev psmisc virtualenv
 apt-get -y install golang go-dep golang-goprotobuf-dev golint
 # install IPFS
 wget https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz
